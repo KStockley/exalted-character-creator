e91cba64c2db872f8c7fae22793235a7c40ed56b
Keiran <keirans@gmail.com>
2018-12-01 15:18:27 -0800

    write a brief outline of the Menu component

---
+++ client/components/Menu.jsx
@@ -0,0 +1,4 @@
+// this view will present the user with the option to create a new character,
+// load an existing character, or delete an existing character
+//
+// three buttons each of which will change the main app view 
