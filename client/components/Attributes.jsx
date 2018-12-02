b11600f7fdc16439678b995bc07f060eb1d3a32a
Keiran <keirans@gmail.com>
2018-12-01 16:43:27 -0800

    add classNames to <p> attributes

--- client/components/Attributes.jsx
+++ client/components/Attributes.jsx
@@ -6,19 +6,19 @@ import PropTypes from 'prop-types';
 const Attributes = (props) => (
   <div className="attributes">
     <div className="physical">
-      <p>Strength: {props.physical.strength}</p>
-      <p>Dexterity: {props.physical.dexterity}</p>
-      <p>Stamina: {props.physical.stamina}</p>
+      <p className="strength">Strength: {props.physical.strength}</p>
+      <p className="dexterity">Dexterity: {props.physical.dexterity}</p>
+      <p className="stamina">Stamina: {props.physical.stamina}</p>
     </div>
     <div className="social">
-      <p>Charisma: {props.social.charisma}</p>
-      <p>Manipulation: {props.social.manipulation}</p>
-      <p>Appearance: {props.social.appearance}</p>
+      <p className="charisma">Charisma: {props.social.charisma}</p>
+      <p className="manipulation">Manipulation: {props.social.manipulation}</p>
+      <p className="appearance">Appearance: {props.social.appearance}</p>
     </div>
     <div className="mental">
-      <p>Perception: {props.mental.perception}</p>
-      <p>Intelligence: {props.mental.intelligence}</p>
-      <p>Wits: {props.mental.wits}</p>
+      <p className="perception">Perception: {props.mental.perception}</p>
+      <p className="intelligence">Intelligence: {props.mental.intelligence}</p>
+      <p className="wits">Wits: {props.mental.wits}</p>
     </div>
   </div>
 );
