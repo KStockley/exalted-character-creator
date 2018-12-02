14cebbf2904ef1387bd11db489d05eb553a4597c
Keiran <keirans@gmail.com>
2018-12-01 16:57:01 -0800

    render basic Abilities component via for...in loop

--- client/components/Abilities.jsx
+++ client/components/Abilities.jsx
@@ -3,22 +3,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Abilities = (props) => (
-  <div className="abilities">
-    <p className="name">
-      Name: {props.abilities.name}
-    </p>
-    <p className="caste">
-      Caste: {props.abilities.caste}
-    </p>
-    <p className="concept">
-      Concept: {props.abilities.concept}
-    </p>
-    <p className="supernal">
-      Supernal Ability: {props.abilities.supernal}
-    </p>
-  </div>
-);
+const Abilities = (props) => {
+  let abilityList = [];
+  
+  for (var ability in props.abilities) {
+    abilityList.push(
+      <p className={ability}>{ability}: {props.abilities[ability]}</p>
+    );
+  }
+
+  return (
+    <div className="abilities">
+      {abilityList}
+    </div>
+  );
+};
 
 Abilities.propTypes = {
   abilities: PropTypes.object.isRequired,
