bf77171a20d97d48de58c3edd74c5a193779c813
Keiran <keirans@gmail.com>
2018-12-03 17:40:26 -0800

    refactor Parry to reflect highest value based on melee/brawl/MA

--- client/components/Defense.jsx
+++ client/components/Defense.jsx
@@ -3,18 +3,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Defense = (props) => (
-  <div className="defense">
-    <fieldset>
-      <legend>Defenses</legend>
-      <div className="soak">Soak: {props.defense.sta}</div>
-      <div className="parry">Parry: {Math.ceil((props.defense.dex + props.defense.melee) / 2)}</div>
-      <div className="evasion">Evasion: {Math.ceil((props.defense.dex + props.defense.dodge) / 2)}</div>
-      <div className="resolve">Resolve: {Math.ceil((props.defense.wits + props.defense.integrity) / 2)}</div>
-      <div className="guile">Guile: {Math.ceil((props.defense.man + props.defense.socialize) / 2)}</div>
-    </fieldset>
-  </div>
-);
+const Defense = (props) => {
+  const parryDef = [props.defense.melee, props.defense.brawl, props.defense.martial].sort()[2];
+  return (
+    <div className="defense">
+      <fieldset>
+        <legend>Defenses</legend>
+        <div className="soak">Soak: {props.defense.sta}</div>
+        <div className="parry">Parry: {Math.ceil((props.defense.dex + (parryDef || 0)) / 2)}</div>
+        <div className="evasion">Evasion: {Math.ceil((props.defense.dex + (props.defense.dodge || 0)) / 2)}</div>
+        <div className="resolve">Resolve: {Math.ceil((props.defense.wits + (props.defense.integrity || 0)) / 2)}</div>
+        <div className="guile">Guile: {Math.ceil((props.defense.man + (props.defense.socialize || 0)) / 2)}</div>
+      </fieldset>
+    </div>
+  );
+};
 
 Defense.propTypes = {
   defense: PropTypes.object.isRequired,
