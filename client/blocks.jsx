369bd6cc69ac94fcc09d01e02990a90ec134ad53
Keiran <keirans@gmail.com>
2018-12-03 17:06:19 -0800

    group related stats into objects for easier management

---
+++ client/blocks.jsx
@@ -0,0 +1,26 @@
+const blank = require('./blank.jsx');
+
+const physical = {
+  Strength: blank.Strength,
+  Dexterity: blank.Dexterity,
+  Stamina: blank.Stamina,
+};
+
+const social = {
+  Charisma: blank.Charisma,
+  Manipulation: blank.Manipulation,
+  Appearance: blank.Appearance,
+};
+
+const mental = {
+  Perception: blank.Perception,
+  Wits: blank.Wits,
+  Intelligence: blank.Intelligence,
+};
+
+const info = {
+  name: blank.name,
+  caste: blank.caste,
+  concept: blank.concept,
+  supernal: blank.supernal,
+};
