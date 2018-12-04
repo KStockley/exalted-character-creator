c23ea8a7be213e1d268e053305e7c90083ae753b
Keiran <keirans@gmail.com>
2018-12-04 10:20:03 -0800

    fill text fields with loaded values

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -7,19 +7,19 @@ const Info = (props) => (
   <div className="info">
     <label>
       Name: 
-      <input type="text" name="name" className="name" onChange={props.handleChange} required />
+      <input type="text" name="name" className="name" value={props.info.name} onChange={props.handleChange} required />
     </label>
     <label>
       Caste: 
-      <input type="text" name="caste" className="caste" onChange={props.handleChange} />
+      <input type="text" name="caste" className="caste" value={props.info.caste} onChange={props.handleChange} />
     </label>
     <label>
       Concept: 
-      <input type="text" name="concept" className="concept" onChange={props.handleChange} />
+      <input type="text" name="concept" className="concept" value={props.info.concept} onChange={props.handleChange} />
     </label>
     <label>
       Supernal Ability: 
-      <input type="text" name="supernal" className="supernal" onChange={props.handleChange} />
+      <input type="text" name="supernal" className="supernal" value={props.info.supernal} onChange={props.handleChange} />
     </label>
   </div>
 );
